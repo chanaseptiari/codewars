4294a78aea6d200fa1301004b941ae11c08f7392
chana <chanaseptiari@gmail.com>
2021-08-10 13:23:54 +0700

    Guess the Sequence Python

--- Python/7_kyu/Guess the Sequence.py
+++ Python/7_kyu/Guess the Sequence.py
@@ -1,11 +1,11 @@
 def sequence(x):
-    # a = []
-    # for i in range(1, x + 1):
-    #     a.append(i)
-    # a.sort(key=lambda x: str(x)[:1])
-    # return a
+    a = []
+    for i in range(1, x + 1):
+        a.append(i)
+    a.sort(key=lambda x: str(x)[:1])
+    return a
 
-    return sorted(range(1, x+1), key=str)
+    # return sorted(range(1, x+1), key=str)
 
 
 print(sequence(16))
