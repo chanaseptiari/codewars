522afbce84d4ee528710ea374c05eb47d4f39e13
chana <chanaseptiari@gmail.com>
2021-08-10 13:29:34 +0700

    Find the smallest integer in the array Python

--- Python/8_kyu/Find the smallest integer in the array.py
+++ Python/8_kyu/Find the smallest integer in the array.py
@@ -6,7 +6,6 @@ def find_smallest_int(arr):
             smallest = arr[i]
     return smallest
 
-
 print(find_smallest_int([78, 56, 232, 12, 11, 43]))
 print(find_smallest_int([78, 56, -2, 12, 8, -33]))
 print(find_smallest_int([0, 1-2**64, 2**64]))
