d2cabff202bc00d168fad96c08ea710e6b89856a
chana <chanaseptiari@gmail.com>
2021-08-10 13:12:53 +0700

    Reversed String C++

---
+++ CPP/src/8_kyu/Reversed String.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string reverseString(string str)
+{
+    // Standar For Looping
+    string tmp;
+    for (int i = str.length() - 1; i >= 0; i--)
+    {
+        tmp.push_back(str[i]);
+    }
+    return tmp;
+}
+
+int main(int argc, char const *argv[])
+{
+    cout << reverseString("hello") << endl;
+    cout << reverseString("rat") << endl;
+    cout << reverseString("alpha") << endl;
+    cout << reverseString("codewars") << endl;
+    cout << reverseString("football") << endl;
+    cout << reverseString("translation") << endl;
+    return 0;
+}
