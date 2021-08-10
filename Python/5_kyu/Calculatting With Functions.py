bee725a53c93fc555fa1c7eda91ac6138c8433da
chana <chanaseptiari@gmail.com>
2021-08-10 13:15:17 +0700

    Calculatting With Function Python

---
+++ Python/5_kyu/Calculatting With Functions.py
@@ -0,0 +1,22 @@
+def zero(num=""): return eval("0"+num)    # your code here
+def one(num=""): return eval("1"+num)    # your code here
+def two(num=""): return eval("2"+num)    # your code here
+def three(num=""): return eval("3"+num)    # your code here
+def four(num=""): return eval("4"+num)    # your code here
+def five(num=""): return eval("5"+num)    # your code here
+def six(num=""): return eval("6"+num)    # your code here
+def seven(num=""): return eval("7"+num)    # your code here
+def eight(num=""): return eval("8"+num)    # your code here
+def nine(num=""): return eval("9"+num)    # your code here
+
+
+def plus(op): return "+" + str(op)  # your code here
+def minus(op): return "-" + str(op)  # your code here
+def times(op): return "*" + str(op)  # your code here
+def divided_by(op): return "//" + str(op)  # your code here
+
+
+print(seven(times(five())))
+print(four(plus(nine())))
+print(eight(minus(three())))
+print(six(divided_by(two())))
