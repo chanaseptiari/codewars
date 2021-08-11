78ac781fd77e1aedb40c2aa93b535a438ade1e69
chana <chanaseptiari@gmail.com>
2021-08-11 22:24:16 +0700

    Beginner Series #2 Clock C++

--- CPP/src/8_kyu/Beginner Series #2 Clock.cpp
+++ CPP/src/8_kyu/Beginner Series #2 Clock.cpp
@@ -6,10 +6,10 @@ int past(int h, int m, int s)
 
 int main(int argc, char const *argv[])
 {
-    std::cout << past(0, 1, 1) << std::endl;
-    std::cout << past(1, 1, 1) << std::endl;
-    std::cout << past(0, 0, 0) << std::endl;
-    std::cout << past(1, 0, 1) << std::endl;
-    std::cout << past(1, 0, 0) << std::endl;
+    std::cout << past(0, 1, 1) << std::endl; // Result 61000
+    std::cout << past(1, 1, 1) << std::endl; // Result 3661000
+    std::cout << past(0, 0, 0) << std::endl; // Result 0
+    std::cout << past(1, 0, 1) << std::endl; // Result 3601000
+    std::cout << past(1, 0, 0) << std::endl; // Result 3600000
     return 0;
 }
