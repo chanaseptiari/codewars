2ea0db64b5f3d5ba6fe10f5cbaf75408a524c28b
chana <chanaseptiari@gmail.com>
2021-08-11 22:22:01 +0700

    Halving Sum

---
+++ CPP/src/7_kyu/Halving Sum.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+unsigned halving_sum(unsigned n)
+{
+    int result = n, i = 1;
+    while (n > 1)
+    {
+        result += n / pow(2, i);
+        n = n / pow(2, i);
+    }
+    return result;
+}
+
+int main(int argc, char const *argv[])
+{
+    std::cout << halving_sum(25) << std::endl;  // Result 47
+    std::cout << halving_sum(127) << std::endl; // Result 247
+    return 0;
+}
