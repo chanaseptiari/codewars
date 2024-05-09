7b26614f54018d312db0d4a7991ed622f208ac7c
chanaseptiari <chanaseptiari@gmail.com>
2024-05-10 01:19:04 +0700

    Codewars Rust Flip

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -7,3 +7,23 @@ fn main() {
 fn hello_test(){
     println!("Hello Unit Test")
 }
+
+fn flip(dir: char, cubes: &[u32]) -> Vec<u32> {
+    let mut cubes = cubes.to_vec();
+
+    if dir == 'R' {
+        cubes.sort();
+    }
+    if dir == 'L' {
+        cubes.sort();
+        cubes.reverse()
+    }
+
+    return cubes;
+}
+
+#[test]
+fn flip_test(){
+    println!("{:?}",flip('R', &vec![3, 2, 1, 2]));
+    println!("{:?}",flip('L', &vec![1, 4, 5, 3, 5]));
+}
