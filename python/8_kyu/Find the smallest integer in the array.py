1803d42700eb0775ef5d9bac64a787ee33a56d86
chanaseptiari <chanaseptiari@gmail.com>
2024-05-18 13:28:12 +0700

    Python: Update Logic find_smallest_int

--- python/8_kyu/Find the smallest integer in the array.py
+++ python/8_kyu/Find the smallest integer in the array.py
@@ -1,10 +1,5 @@
 def find_smallest_int(arr):
-    # return min(arr)
-    smallest = []
-    for i in range(0, len(arr)):
-        if (arr[i] < smallest):
-            smallest = arr[i]
-    return smallest
+    return min(arr)
 
 print(find_smallest_int([78, 56, 232, 12, 11, 43]))
 print(find_smallest_int([78, 56, -2, 12, 8, -33]))
