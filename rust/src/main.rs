7ec788997ca5f296f02fd99ae858cdd923e1004e
Chana S O <ChanaSeptiari@gmail.com>
2024-10-27 01:11:22 +0700

    Rust: count_by

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,10 +1,9 @@
 fn main() {
     println!("Hello, world!");
-    
 }
 
 #[test]
-fn hello_test(){
+fn hello_test() {
     println!("Hello Unit Test");
 }
 
@@ -23,20 +22,21 @@ fn flip(dir: char, cubes: &[u32]) -> Vec<u32> {
 }
 
 #[test]
-fn flip_test(){
-    println!("{:?}",flip('R', &vec![3, 2, 1, 2]));
-    println!("{:?}",flip('L', &vec![1, 4, 5, 3, 5]));
+fn flip_test() {
+    println!("{:?}", flip('R', &vec![3, 2, 1, 2]));
+    println!("{:?}", flip('L', &vec![1, 4, 5, 3, 5]));
 }
 
 fn sum_or_product(list: &[i64], n: usize) -> String {
     // println!("\n{}\n",n);
-    let mut arr = list.to_vec();arr.sort();
-    let product:i64 = arr[..n].iter().product();
-    let sum:i64 = arr[arr.len()-n..].iter().sum();
-    
+    let mut arr = list.to_vec();
+    arr.sort();
+    let product: i64 = arr[..n].iter().product();
+    let sum: i64 = arr[arr.len() - n..].iter().sum();
+
     // println!("{:?} {:?}",arr[..n].iter(),arr[arr.len()-n..].iter());
     // println!("{:?} {:?}",product,sum);
-    
+
     if sum > product {
         "sum".to_string()
     } else if sum < product {
@@ -47,28 +47,27 @@ fn sum_or_product(list: &[i64], n: usize) -> String {
 }
 
 #[test]
-fn sum_or_product_test(){
-    println!("{}",sum_or_product(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 4));
-    println!("{}",sum_or_product(&[10, 41, 8, 16, 20, 36, 9, 13, 20], 3));
-    println!("{}",sum_or_product(&[10, 20, 3, 30, 5, 4], 3));
-    
+fn sum_or_product_test() {
+    println!("{}", sum_or_product(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 4));
+    println!("{}", sum_or_product(&[10, 41, 8, 16, 20, 36, 9, 13, 20], 3));
+    println!("{}", sum_or_product(&[10, 20, 3, 30, 5, 4], 3));
 }
 
 fn odd_count(n: u64) -> u64 {
-    // let mut arr=vec![]; 
+    // let mut arr=vec![];
     // for i in 1..n {
     //     if i % 2 == 1 {
     //         arr.push(i)
     //     }
     // }
     // return arr.len() as u64;
-    n/2
+    n / 2
 }
 
 #[test]
-fn odd_count_test(){
-    println!("{}",odd_count(15));
-    println!("{}",odd_count(15023));
+fn odd_count_test() {
+    println!("{}", odd_count(15));
+    println!("{}", odd_count(15023));
 }
 
 // fn unique_in_order<T>(sequence:T)
@@ -92,19 +91,50 @@ where
 
 #[test]
 fn unique_in_order_test() {
-    println!("{:?}",unique_in_order("AAAABBBCCDAABBB".chars()));
-    println!("{:?}",unique_in_order("ABBCcAD".chars()));
+    println!("{:?}", unique_in_order("AAAABBBCCDAABBB".chars()));
+    println!("{:?}", unique_in_order("ABBCcAD".chars()));
 }
 
-
 fn find_missing_letter(chars: &[char]) -> char {
-    let arr: Vec<char> = (chars[0] as u8..=chars[chars.len()-1] as u8).into_iter().map(|x| x as char).collect();
-    let diff: Vec<char> = arr.iter().cloned().filter(|x|!chars.contains(x)).collect();
+    let arr: Vec<char> = (chars[0] as u8..=chars[chars.len() - 1] as u8)
+        .into_iter()
+        .map(|x| x as char)
+        .collect();
+    let diff: Vec<char> = arr.iter().cloned().filter(|x| !chars.contains(x)).collect();
     diff[0]
 }
 
 #[test]
 fn find_missing_letter_tests() {
-    println!("{}",find_missing_letter(&['a', 'b', 'c', 'd', 'f']));
-    println!("{}",find_missing_letter(&['O', 'Q', 'R', 'S']));
+    println!("{}", find_missing_letter(&['a', 'b', 'c', 'd', 'f']));
+    println!("{}", find_missing_letter(&['O', 'Q', 'R', 'S']));
+}
+
+fn count_by(x: u32, n: u32) -> Vec<u32> {
+    // let mut res: Vec<u32> = Vec::new();
+
+    // for index in 1 ..= n {
+    //     res.push(index * x as u32);
+    //     // res.push(n)
+    // }
+
+    // res
+
+    (1 ..= n).map(|a|a*x).collect()
+}
+
+#[test]
+fn count_by_text() {
+    println!("{:?}", count_by(1, 5));
+    println!("{:?}", count_by(1, 10));
+    println!("{:?}", count_by(2, 5));
+    println!("{:?}", count_by(3, 7));
+    println!("{:?}", count_by(50, 5));
+    println!("{:?}", count_by(100, 6));
+
+    // assertion(vec![1,2,3,4,5,6,7,8,9,10], (1, 10));
+    // assertion(vec![2,4,6,8,10], (2, 5));
+    // assertion(vec![3,6,9,12,15,18,21], (3, 7));
+    // assertion(vec![50,100,150,200,250], (50, 5));
+    // assertion(vec![100,200,300,400,500,600], (100, 6));
 }
